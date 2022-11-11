350f3690b8042a3967f2bf26d015a2cb7c9cf669
jeffrey hernandez <hernandezjeffrey2510@gmail.com>
2022-11-10 20:41:52 -0800

    updated navigation

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,16 +1,90 @@
-import react from 'react'
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const NavigationContainer = styled.div`
+justify-content: space-evenly;
+display: flex;
+flex-direction: row;
+background-color: blue;
+padding-top: 5px
+`;
+
+const NavigationButtons = styled.div`
+background-color: red;
+border: solid;
+border-color: white;
+border-width: 2px;
+border-radius: 7px;
+margin-bottom: 5px;
+`;
+
+const Header = styled.h1`
+text-align: center;
+`;
 
 export default function Home() {
 
     return (
         <div>
-            <div style={{flexDirection: 'column', display: 'flex'}}>
-            <h1> this is the home page </h1>
-            <Link to='/team'>Team</Link>
-            <Link to='/update-pokemon'>Update</Link>
-            <Link to='/add-pokemon'>New Pokemon Entry</Link>
-            </div>
+            <NavigationContainer>
+                <NavigationButtons>
+                    <Link
+                        style={{
+                            color: 'white',
+                            textDecoration: 'none',
+                            paddingLeft: 10,
+                            paddingRight: 10,
+                            paddingTop: 5,
+                            paddingBottom: 5,
+                        }}
+                        to='/'>
+                        Home
+                    </Link>
+                </NavigationButtons>
+                <NavigationButtons>
+                    <Link
+                        style={{
+                            color: 'white',
+                            textDecoration: 'none',
+                            paddingLeft: 10,
+                            paddingRight: 10,
+                            paddingTop: 5,
+                            paddingBottom: 5,
+                        }}
+                        to='/team'>
+                        Team
+                    </Link>
+                </NavigationButtons>
+                <NavigationButtons>
+                    <Link 
+                    style={{
+                        color: 'white',
+                        textDecoration: 'none',
+                        paddingLeft: 10,
+                        paddingRight: 10,
+                        paddingTop: 5,
+                        paddingBottom: 5,
+                    }}
+                    to='/update-pokemon'>
+                        Update
+                        </Link>
+                </NavigationButtons>
+                <NavigationButtons>
+                    <Link 
+                    style={{
+                        color: 'white',
+                        textDecoration: 'none',
+                        paddingLeft: 10,
+                        paddingRight: 10,
+                        paddingTop: 5,
+                        paddingBottom: 5,
+                    }}
+                    to='/add-pokemon'>
+                        New Pokemon
+                        </Link>
+                </NavigationButtons>
+            </NavigationContainer>
+            <Header> Pokemon </Header>
         </div>
     )
 }
